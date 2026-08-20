9ac62cff5a465baf3bf6e24e43334b767df4aa5c
den3099 <dennisarce99@gmail.com>
2026-08-19 18:05:12 -0600

    Se creó el módulo de Verificador de Golpe

---
+++ build/hit_verifier.sv
@@ -0,0 +1,38 @@
+module hit_verifier(
+    input  logic [7:0] boton_presionado,
+    input  logic [7:0] led_encendido,
+    output logic       golpe_correcto,
+    output logic       golpe_incorrecto
+);
+
+    logic comparacion;
+
+    always_comb begin
+        comparacion = (boton_presionado == led_encendido);
+        switch (boton_presionado) begin
+            8'b0000_0001,
+            8'b0000_0010,
+            8'b0000_0100,
+            8'b0000_1000,
+            8'b0001_0000,
+            8'b0010_0000,
+            8'b0100_0000,
+            8'b1000_0000: begin
+                if (comparacion) begin
+                    golpe_correcto = 1'b1;
+                    golpe_incorrecto = 1'b0;
+                end else begin
+                    golpe_correcto = 1'b0;
+                    golpe_incorrecto = 1'b1;
+                end
+            end
+
+            default: begin
+                golpe_correcto = 1'b0;
+                golpe_incorrecto = 1'b0;
+            end
+        end
+
+    end
+
+endmodule
